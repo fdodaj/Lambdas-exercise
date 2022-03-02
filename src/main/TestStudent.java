705ee9b49b91fc52c9e815154e5c142c969e3e5d
Florian Dodaj <floriandodaj55@gmail.com>
2022-03-02 19:22:33 +0100

    Used BiPredicate to compare two students grades

--- src/main/TestStudent.java
+++ src/main/TestStudent.java
@@ -21,6 +21,32 @@ public class TestStudent {
         students.add(new Student("Stuart", "Mark", 15, " second year ", 9));
         // 700 students;
 
+        //BiPredicate to find if student "a" has a better grade than student "b"
+        BiPredicate<Integer, Integer> filter = (a, b) -> {
+            return a < b;};
+
+
+        System.out.println("index |   Student");
+        System.out.println("----------------");
+        for (int counter = 0; counter < students.size(); counter++) {
+            System.out.print(counter);
+            System.out.print("     |   ");
+            System.out.print(students.get(counter).getName());
+            System.out.println();
+
+
+        }
+        System.out.println("Enter two students indexes you want to compare");
+        int student1 = SCANNER.nextInt();
+        int student2 = SCANNER.nextInt();
+        boolean compare = filter.test(students.get(student1).getGrade(), students.get(student2).getGrade());
+        if (compare == false){
+            System.out.println(students.get(student2).getName() +"(" + students.get(student2).getGrade() + ")"   + " has a worse grade than " + students.get(student1).getName() + "(" + students.get(student1).getGrade() + ")" );  // true
+        }
+        else {
+            System.out.println(students.get(student1).getName()+ "(" + students.get(student1).getGrade() + ")" + " has a worse grade than " + students.get(student2).getName()+"(" + students.get(student2).getGrade() + ")"    );  // true
+        }
+
 
         //add a new Student using the Suplier
         Supplier studentSupplier = () -> students.add(new Student("Test", "Test", 16, " first year ", 10));
@@ -58,12 +84,14 @@ public class TestStudent {
         System.out.println("The school has a total of " + numberOfStudents.apply(students.size()) + " students.");
 
     }
+
     private void failClass(List<Student> students, Predicate<Student> predicate){
         for (Student student: students){
             if (predicate.test(student))
                 System.out.println(student.getName() + " " + student.getSurname() + " failed " + student.getYear());
         }
     }
+
 
     static void bestStudents(List<Student> students, Consumer<Student> consumer) {
         for (Student student : students) {
